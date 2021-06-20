f62ac252f3c67fb3637c76797606a9334c1efd15
Santiago Sarchetti <saaantiii@github.com>
2021-06-20 01:26:33 -0300

    Contenido Responsive

--- ProyectoFinal/js/main.js
+++ ProyectoFinal/js/main.js
@@ -1,5 +1,5 @@
 const btntoggle = document.querySelector('.toggle-btn');
 console.log (btntoggle);
 btntoggle.addEventListener('click', function() {
-    document.getElementById('mySidebar').classList.toggle('active');
+    document.getElementById('sidebar').classList.toggle('active');
 });
